37091a29d31526956cad06b1b99c95e1b0c09ca4
100472248 <100472248@alumnos.uc3m.es>
2023-02-16 18:42:42 +0100

    nuevos tests

--- UC3MLogistics/OrderManager.py
+++ UC3MLogistics/OrderManager.py
@@ -7,21 +7,21 @@ class OrderManager:
         pass
 
     def ValidateEAN13(self, EAN13):
-        """Para examinar si el código indicado cumple las pautas necesarias para que sea del tipo eAn13"""
+        """Para examinar si el código indicado es del tipo eAn13"""
         # Si la longitud del código es distinta de 13 dígitos, no es de tipo EAN13
         if len(EAN13) != 13:
             return False
-        # Los numeros en posiciones impares de los 13 digitos estan ponderados con un 3.
-        # Para comprobar si el codigo es adecuado primero tenemos que realizar la suma de todos los digitos
-        # multiplicándolos además por 3 los que están ponderados
+        # Primero sumamos los números de las posiciones impares (menos la número 13)
         impares = EAN13[0] + EAN13[2] + EAN13[4] + EAN13[6] + EAN13[8] + EAN13[10]
+        # Lo mismo pero ahora con todos los de las pares
         pares = EAN13[1] + EAN13[3] + EAN13[5] + EAN13[7] + EAN13[9] + EAN13[11]
+        # Multiplicamos el resultado de los impares por 3 y lo sumamos a los pares
         suma = impares*3 + pares
-        # Ahora, tenemos que coger esa suma y hallar la resta del numero múltiplo de 10 igual o superior a
+        # Ahora, tenemos que coger esa suma y hallar la resta del número múltiplo de 10 igual o superior a
         # dicho número menos ese número, es decir cuanto falta para llegar del número hasta el múltiplo
         resto = suma % 10
         num = 10 - resto
-        # Si el digito 13 del dódigo no es igual al número calculado, el código no cumple el estándar
+        # Si el dígito 13 del código no es igual al número calculado, el código no cumple el estándar
         if num != EAN13[12]:
             return False
         return True
